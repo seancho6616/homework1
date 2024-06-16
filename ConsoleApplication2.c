c07d9f6ee95aeb533962a540d4a9e785e0c9c1ae
seancho6616 <129930818+seancho6616@users.noreply.github.com>
2024-06-16 14:38:25 +0900

    Add files via upload

--- ConsoleApplication2.c
+++ ConsoleApplication2.c
@@ -18,91 +18,93 @@
 #define LINE 50
 #define BUF_SIZE 100
 
-//unsigned WINAPI SendMsg(void* arg);//쓰레드 전송함수
-//unsigned WINAPI RecvMsg(void* arg);//쓰레드 수신함수
+unsigned WINAPI SendMsg(void* arg);//쓰레드 전송함수
+unsigned WINAPI RecvMsg(void* arg);//쓰레드 수신함수
 
-//int main(int argc, char* argv[]) {
-//	WSADATA wsaData;
-//	SOCKET sock;
-//	SOCKADDR_IN serverAddr;
-//	HANDLE sendThread, recvThread;
-//
-//	char serverIp[100];
-//	int  portNum;
-//
-//	printf("Input server IP : ");
-//	gets_s(serverIp);
-//
-//	printf("Input server port : ");
-//	scanf("%d", &portNum);
-//
-//	WSAStartup(MAKEWORD(2, 2), &wsaData);
-//	sock = socket(PF_INET, SOCK_STREAM, 0);//소켓을 하나 생성한다.
-//
-//	memset(&serverAddr, 0, sizeof(serverAddr));
-//	serverAddr.sin_family = AF_INET;
-//	serverAddr.sin_addr.s_addr = inet_addr(serverIp);
-//	serverAddr.sin_port = htons(portNum);
-//
-//	connect(sock, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
-//	//접속에 성공하면 이 줄 아래가 실행된다.
-//
-//	sendThread = (HANDLE)_beginthreadex(NULL, 0, SendMsg, (void*)&sock, 0, NULL);//메시지 전송용 쓰레드가 실행된다.
-//	recvThread = (HANDLE)_beginthreadex(NULL, 0, RecvMsg, (void*)&sock, 0, NULL);//메시지 수신용 쓰레드가 실행된다.
-//
-//	WaitForSingleObject(sendThread, INFINITE);//전송용 쓰레드가 중지될때까지 기다린다./
-//	WaitForSingleObject(recvThread, INFINITE);//수신용 쓰레드가 중지될때까지 기다린다.
-//	//클라이언트가 종료를 시도한다면 이줄 아래가 실행된다.
-//	closesocket(sock);//소켓을 종료한다.
-//	WSACleanup();//윈도우 소켓 사용중지를 운영체제에 알린다.
-//
-//	return 0;
-//}
-//
-//unsigned WINAPI SendMsg(void* arg) {//전송용 쓰레드함수
-//	SOCKET sock = *((SOCKET*)arg);//서버용 소켓을 전달한다.
-//	char msg[BUF_SIZE] = "";
-//	while (1) {//반복
-//		gets_s(msg);//입력을 받는다.
-//		if (!strcmp(msg, "q")) {//q를 입력하면 종료한다.
-//			send(sock, "q", 1, 0);//Msg를 서버에게 전송한다.
-//			strcpy(msg, "");
-//		}
-//		else {
-//			send(sock, msg, strlen(msg), 0);//Msg를 서버에게 전송한다.
-//			strcpy(msg, "");
-//		}
-//
-//	}
-//	return 0;
-//}
-//
-//unsigned WINAPI RecvMsg(void* arg) {
-//	SOCKET sock = *((SOCKET*)arg);//서버용 소켓을 전달한다.
-//	char msg[BUF_SIZE] = "";
-//	int strLen;
-//	while (1) {//반복
-//		strLen = recv(sock, msg, BUF_SIZE - 1, 0);//서버로부터 메시지를 수신한다.
-//		if (strLen == -1)
-//			return -1;
-//		msg[strLen] = '\0';//문자열의 끝을 알리기 위해 설정
-//		if (!strcmp(msg, "q")) {
-//			printf("Client : Disconnection\n");
-//			closesocket(sock);
-//			exit(0);
-//		}
-//		else {
-//			printf("Result : %s\n", msg);
-//		}
-//		strcpy(msg, "");
-//	}
-//	return 0;
-//}
+int main(int argc, char* argv[]) {
+	
+	ipInterface();
 
-void main()
-{
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	sock = socket(PF_INET, SOCK_STREAM, 0);//소켓을 하나 생성한다.
+
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = inet_addr(serverIp);
+	serverAddr.sin_port = htons(portNum);
+
+	connect(sock, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+	//접속에 성공하면 이 줄 아래가 실행된다.
+
 	while (1) {
 		mainMenuInterface();
 	}
-	//addInformation();
+
+	sendThread = (HANDLE)_beginthreadex(NULL, 0, SendMsg, (void*)&sock, 0, NULL);//메시지 전송용 쓰레드가 실행된다.
+	recvThread = (HANDLE)_beginthreadex(NULL, 0, RecvMsg, (void*)&sock, 0, NULL);//메시지 수신용 쓰레드가 실행된다.
+
+	WaitForSingleObject(sendThread, INFINITE);//전송용 쓰레드가 중지될때까지 기다린다./
+	WaitForSingleObject(recvThread, INFINITE);//수신용 쓰레드가 중지될때까지 기다린다.
+	//클라이언트가 종료를 시도한다면 이줄 아래가 실행된다.
+	closesocket(sock);//소켓을 종료한다.
+	WSACleanup();//윈도우 소켓 사용중지를 운영체제에 알린다.
+
+	return 0;
 }
+
+unsigned WINAPI SendMsg(void* arg) {//전송용 쓰레드함수
+	SOCKET sock = *((SOCKET*)arg);//서버용 소켓을 전달한다.
+	char msg[BUF_SIZE] = "";
+	while (1) {//반복
+		switch (c) {
+			case 'c':
+			case 'n':
+			case 'd':
+			case 'o':
+				sprintf(msg, "%c/%s", c, word);
+				send(sock, msg, strlen(msg), 0);
+				strcpy(msg, "");
+				break;
+			case 'a':
+				sprintf(msg, "%c/%d", c, num);
+				send(sock, msg, strlen(msg), 0);
+				strcpy(msg, "");
+				break;
+			case 'f':
+				sprinf(msg, "%s/%d/%s/%s",
+					student.name, student.age, student.nation, student.depart);
+				send(sock, msg, strlen(msg), 0);
+				strcpy(msg, "");
+				break;
+			case 'q':
+				send(sock, "q", 1, 0);
+				strcpy(msg, "");
+				break;
+		}
+	}
+	return 0;
+}
+
+unsigned WINAPI RecvMsg(void* arg) {
+	SOCKET sock = *((SOCKET*)arg);//서버용 소켓을 전달한다.
+	char msg[BUF_SIZE] = "";
+	int strLen;
+	while (1) {//반복
+		strLen = recv(sock, msg, BUF_SIZE - 1, 0);//서버로부터 메시지를 수신한다.
+		if (strLen == -1)
+			return -1;
+		msg[strLen] = '\0';//문자열의 끝을 알리기 위해 설정
+		if (!strcmp(msg, "q")) {
+			printf("Client : Disconnection\n");
+			closesocket(sock);
+			exit(0);
+		}
+		else {
+			printf("Result : %s\n", msg);
+		}
+		strcpy(msg, "");
+	}
+	return 0;
+}
+
+
--- students.c
+++ students.c
@@ -20,54 +20,63 @@ char c1 = 'h';
 ST student;			// 클라이언트에서 사용할 구조체
 ST* stInfor=NULL;			// 서버에서 받은 정보 저장
 
+// 텍스트 색 변경 함수
 void textcolor(int colorNum) {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorNum);
 }
 
-//enum ColorType { RED = 12, BLUE = 9, GREEN = 10, YELLOW = 14, WHITE = 15 }COLOR;
 
+// 위치 이동 함수
 void gotoxy(int x, int y) {
 	COORD pos = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
+// IP와 Port번호 받기 위한 인터페이스 함수
 void ipInterface() {
+	// 콘솔 창 지우기
 	system("cls");
-	gotoxy(3, 2);	printf("IP : ");
-	textcolor(YELLOW);
-	gotoxy(3, 3);	printf("Enter IP of server");
-	textcolor(WHITE);
-	gotoxy(3, 5);	printf("Port : ");
-	textcolor(YELLOW);
-	gotoxy(3, 6);	printf("Enter Port of server");
-	textcolor(WHITE);
+	// 인터페이스
+	gotoxy(3, 2);	printf("IP : \n");
+	textcolor(YELLOW);		// 출력 텍스트 색 변경 ( 노랑 )
+	gotoxy(3, 3);	printf("Enter IP of server\n");
+	textcolor(WHITE);		// 출력 텍스트 색 변경 ( 흰색 )
+	gotoxy(3, 5);	printf("Port : \n");
+	textcolor(YELLOW);		// 출력 텍스트 색 변경 ( 노랑 )
+	gotoxy(3, 6);	printf("Enter Port of server\n");
+	textcolor(WHITE);		// 출력 텍스트 색 변경 ( 흰색 )
 
+	// 입력 받는 변수들
 	gotoxy(8, 2); gets(serverIp);
 	gotoxy(10, 5); scanf("%d", &portNum);
 }
 
-void mainMenuInterface() {
+// 메인메뉴 인터페이스 함수
+void mainMenuInterface() {	
+	// 콘솔 창 지우기
 	system("cls");
+	// 메인 메뉴 인터페이스
 	gotoxy(2, 1); printf("Searching to Students Information \n\n");
 	for (int i = 0; i <= LINE; i++) {
-		textcolor(GREEN);
+		textcolor(GREEN);	// 출력 텍스트 색 변경 ( 초록 )
 		printf("=");
 	}
-	textcolor(WHITE);
+	textcolor(WHITE);		// 출력 텍스트 색 변경 ( 흰색 )
 	gotoxy(2, 5);		printf("1. Search for a Name\n");
 	gotoxy(2, 7);		printf("2. Search by Nation\n");
 	gotoxy(2, 9);		printf("3. Search by Department\n");
 	gotoxy(2, 11);	printf("4. Search by Age\n");
 	gotoxy(2, 13);	printf("5. Add or Delet Information\n");
 	gotoxy(2, 15);	printf("6. Close\n");
-	searchingInterface();
-	//c1 = 'h';
+	searchingInterface();	//  검색 인터페이스 창으로 이동
 }
 
+// 검색 인터페이스 창 함수
 void searchingInterface() {
-	fflush(stdin);
-	gotoxy(3, 18);	printf(">  ");	scanf("%d", &num);
-	if (num == 6) {
+	fflush(stdin);	// 버퍼 초기화
+	gotoxy(3, 18);	printf(">  ");	scanf("%d", &num);	// 목록 선택
+	if (num == 6) {		// 6 선택시 종료
+		c = 'q';
 		system("cls");
 		gotoxy(8, 3);
 		textcolor(YELLOW);	printf("END\n\n\n");	textcolor(WHITE);
@@ -77,44 +86,66 @@ void searchingInterface() {
 	switch (num) {
 		case 1:
 			gotoxy(3, 20);	printf("Enter a name >  "); gets(word); 
-			findName();
+			c = 'c';
+			findName();		// 이름 검색 인터페이스로 이동
 			system("pause");
 			break;
 		case 2:
 			gotoxy(3, 20);	printf("Enter a nation >  "); gets(word);
-			findNation();
+			c = 'n';
+			findNation();		// 나라 검색 인터페이스로 이동
 			system("pause");
 			break;
 		case 3:
 			gotoxy(3, 20);	printf("Enter a depertment >  "); gets(word);
-			findDepartment();
+			c = 'd';
+			findDepartment();	// 전공 검색 인터페이스로 이동
 			system("pause");
 			break;
 		case 4:
 			gotoxy(3, 20);	printf("Enter an age >  "); scanf("%d", &num);
-			findAge();
+			c = 'a';
+			findAge();				// 나이 검색 인터페이스로 이동
 			system("pause");
 			break;
 		case 5:
-			information();
+			information();			// 정보 추가 또는 제거 인터페이스
 			system("pause");
 			break;
-		default:
-			printf("Enter again");
+		default:		// 잘못 입력할 시
+			textcolor(RED);
+			printf("Enter again\n");
+			textcolor(WHITE);
 			system("pause");
 			break;
 	}
 }
 
+// 정보 추가 또는 제거 인터페이스 함수
 void information() {
 	system("cls");
 	fflush(stdin);
-	gotoxy(3, 2);	printf("1. Add information  ");
-	gotoxy(3, 4);	printf("2. Delet information  ");
+	gotoxy(3, 2);	printf("1. Add information  \n");
+	gotoxy(3, 4);	printf("2. Delet information  \n");
 	printf("\n");
 	gotoxy(5, 6); printf(">  ");	 scanf("%d", &num);
+	getchar();
+	switch (num) {
+		case 1:
+			addInformation();	// 정보 추가 인터페이스로 이동
+			break;
+		case 2:
+			delInformation();	// 정보 삭제 인터페이스로 이동
+			break;
+		default:		// 잘못 입력할 시 
+			gotoxy(7, 7);	textcolor(RED);
+			printf("Wrong");
+			textcolor(WHITE);
+			break;
+	}
 }
 
+// 이름 검색 인터페이스 함수
 void findName() {
 	system("cls");
 	fflush(stdin);
@@ -122,12 +153,14 @@ void findName() {
 	for (int i = 0; i <= LINE; i++) {
 		printf("-");
 	}
-	gotoxy(3, 3);	printf("Name"); 
-	gotoxy(10, 3);	printf("Age");
-	gotoxy(16, 3);	printf("Nation");
+	gotoxy(3, 3);	printf("Name\n"); 
+	gotoxy(10, 3);	printf("Age\n");
+	gotoxy(16, 3);	printf("Nation\n");
 	gotoxy(26, 3);	printf("Department\n");
+	printf("\n");
 }
 
+// 나라 검색 인터페이스 함수
 void findNation() {
 	system("cls");
 	fflush(stdin);
@@ -135,9 +168,10 @@ void findNation() {
 	for (int i = 0; i <= LINE; i++) {
 		printf("-");
 	}
-	printf("\n");
+	printf("\n\n");
 }
 
+// 전공 검색 인터페이스 함수
 void findDepartment() {
 	system("cls");
 	fflush(stdin);
@@ -145,9 +179,10 @@ void findDepartment() {
 	for (int i = 0; i <= LINE; i++) {
 		printf("-");
 	}
-	printf("\n");
+	printf("\n\n");
 }
 
+// 나이 검색 인터페이스 함수
 void findAge() {
 	system("cls");
 	fflush(stdin);
@@ -155,14 +190,36 @@ void findAge() {
 	for (int i = 0; i <= LINE; i++) {
 		printf("-");
 	}
-	printf("\n");
+	printf("\n\n");
 }
 
+// 정보 추가 인터페이스 함수
 void addInformation() {
-	gotoxy(2, 1); printf("Name : ");
-	gotoxy(2, 3); printf("Age : ");
-	gotoxy(2, 5); printf("Nation : ");
-	gotoxy(2, 7); printf("Department : ");
-	//gotoxy()
+	system("cls");
+	fflush(stdin);
+	gotoxy(2, 1); printf("+ Name : \n");
+	gotoxy(2, 3); printf("+ Age : \n");
+	gotoxy(2, 5); printf("+ Nation : \n");
+	gotoxy(2, 7); printf("+ Department : \n");
+	// 입력
+	gotoxy(11, 1); gets(student.name);
+	gotoxy(10, 3); scanf("%d", &student.age);
+	getchar();
+	gotoxy(13, 5); gets(student.nation);
+	gotoxy(17, 7); gets(student.depart);
+	printf("\n");
+	c = 'f';
+}
 
+// 정보 삭제 인터페이스 함수
+void delInformation() {
+	system("cls");
+	fflush(stdin); 
+	gotoxy(2, 1);	printf("Enter a name to delete\n");
+	gotoxy(3, 3);	printf(">  ");	gets(word);
+	textcolor(YELLOW);
+	gotoxy(8, 5);	printf("+ Delete +\n");
+	textcolor(WHITE);
+	printf("\n");
+	c = 'o';
 }
